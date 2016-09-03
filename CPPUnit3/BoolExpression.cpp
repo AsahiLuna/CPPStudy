786cedd916dbe23c49103d8ff1ba92d65244c5cd
张泽旭 <542792296@qq.com>
2016-09-03 09:57:33 +0800

    Change to function

--- CPPUnit3/BoolExpression.cpp
+++ CPPUnit3/BoolExpression.cpp
@@ -2,13 +2,18 @@
 using std::cin;
 using std::cout;
 using std::endl;
+void tureFalseTest();
 int main() {
+	tureFalseTest();
+	return 0;
+}
+void tureFalseTest() {
 	int a = 0, b = 1;
 	cout << (3 == a) << endl;
 	cout << (b < a) << endl;
 	cout << (3.2 > b) << endl;
 	cout << (a&&b) << endl;
-	cout << (b||18) << endl;
+	cout << (b || 18) << endl;
 	cout << (!a) << endl;
-	return 0;
+
 }

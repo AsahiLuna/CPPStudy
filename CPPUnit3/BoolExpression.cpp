671355909077d031012740c8e0ba2bed2e381881
张泽旭 <542792296@qq.com>
2016-09-03 09:52:50 +0800

    false&true

---
+++ CPPUnit3/BoolExpression.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+using std::cin;
+using std::cout;
+using std::endl;
+int main() {
+	int a = 0, b = 1;
+	cout << (3 == a) << endl;
+	cout << (b < a) << endl;
+	cout << (3.2 > b) << endl;
+	cout << (a&&b) << endl;
+	cout << (b||18) << endl;
+	cout << (!a) << endl;
+	return 0;
+}
